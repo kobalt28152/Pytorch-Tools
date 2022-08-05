3e7162d3faf8b9ee22b0ae7afa06d152b7a298ea
kobalt <direccionfalsa0123@gmail.com>
2022-08-05 17:05:04 -0500

    Modify AverageMeter to work with torch.Tensor
    Modify AverageMeter class such that it now works with torch.Tensor
    instead of float. This allows it to record metrics that return
    vectors instead of scalars (e.g., for multi-class segmentation).

--- pytorch_tools/metrics/meter.py
+++ pytorch_tools/metrics/meter.py
@@ -17,17 +17,22 @@ class AverageMeter(object):
 
     def update(self, val, n=1):
         """ 
-        val : float
+        Parameters
+        ----------
+        val : torch.Tensor
             Average metric of a batch; e.g., (a1 + a2 + ... + an)/n
         n : int
             The batch size
-        sum : float
+
+        Using the previous paremters, then modify:
+
+        sum : torch.Tensor
             The total sum of all batch elements
                 sum += val * n -> a1 + a2 + a3 + ...
         count : int
             The total number of elements proccessed sofar
                 count += n
-        avg : float
+        avg : torch.Tensor
             The total average
                 avg = sum / count
         """
@@ -35,12 +40,22 @@ class AverageMeter(object):
         self.sum += val * n    # total sum
         self.count += n        # total number of elements
         self.avg = self.sum / self.count    # total average
-        
+
     def __str__(self):
-        if self.name:
-            return f'({self.name} meter) total avg: {self.sum:.5f}/{self.count} = {self.avg:.5f}'
+        if self.count == 0:
+            return f'Uninitialized meter'
+
+        if self.avg.size() == ():    # No dimensions (scalar)
+            s = f'{self.sum:.5f}/{self.count} = {self.avg:.5f}'
         else:
-            return f'Total avg: {self.sum:.5f}/{self.count} = {self.avg:.5f}'
-        
+            tmp1 = ', '.join([f'{a:.5f}' for a in self.sum])
+            tmp2 = ', '.join([f'{a:.5f}' for a in self.avg])
+            s = f'({tmp1})/{self.count} = ({tmp2})'
+
+        if self.name:
+            return f'({self.name} meter) total avg: {s} '
+        else:
+            return f'Total avg: {s}'
+
     def __repr__(self):
         return self.__str__()

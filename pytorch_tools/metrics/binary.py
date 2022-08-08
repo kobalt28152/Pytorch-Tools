821374ca71513bf4bf41054f6b0ec51554f5e734
kobalt <direccionfalsa0123@gmail.com>
2022-08-08 01:13:12 -0500

    Change metrics to work only on torch.Tensor

--- pytorch_tools/metrics/binary.py
+++ pytorch_tools/metrics/binary.py
@@ -1,48 +1,54 @@
-import numpy as np
 import torch
 
 
 # ========== Individual metrics ========
 
-def IoU(a, b, eps=1e-6):
+def IoU(a, b):
     """ IoU score
 
     Parameters
     ----------
-    a : bool
-        H x W image
-    b : bool
-        H x W image
+    a : torch.Tensor
+        binary image; N x H x W or H x W
+    b : torch.Tensor
+        binary image; N x H x W or H x W
 
     Returns
     -------
-    float
-        Iou score between a, b.
+    torch.Tensor
+        Iou score between a, b; (N, 1) or () tensor
     """
-    inter = (a*b).sum()
-    union = a.sum() + b.sum() - inter
+    inter = torch.sum(a*b, dim=(-2,-1))
+    card = torch.sum(a, dim=(-2,-1)) + torch.sum(b, dim=(-2,-1))
 
-    return inter / (union + 1)    # Add 1 to avoid division by 0
+    # union = a.sum() + b.sum() - inter
+    ret = inter / (card - inter + 1)    # Add 1 to avoid division by 0
+    ret[card == 0] = 1.0                # when a and b are empty card == 0
 
-def Dice(a, b, eps=1e-6):
+    return ret
+
+def Dice(a, b):
     """ Dice score
 
     Parameters
     ----------
-    a : bool
-        H x W image
-    b : bool
-        H x W image
+    a : torch.Tensor
+        binary image; N x H x W or H x W
+    b : torch.Tensor
+        binary image; N x H x W or H x W
 
     Returns
     -------
-    float
-        Dice score between a, b.
+    torch.Tensor
+        Dice score between a, b; (N, 1) or () tensor
     """
-    inter = (a*b).sum()
-    card = a.sum() + b.sum()
+    inter = torch.sum(a*b, dim=(-2,-1))
+    card = torch.sum(a, dim=(-2,-1)) + torch.sum(b, dim=(-2,-1))
+            
+    ret = (2.0*inter) / (card + 1)    # +1 to avoid division by zero
+    ret[card == 0] = 1.0              # when a and b are empty card == 0
 
-    return (2.0*inter) / (card + 1)    # Add 1 to avoid division by 0
+    return ret
 
 
 # ========== Batch metrics ========
@@ -52,7 +58,7 @@ class IoU_Metric(object):
 
         IoU(a, b) = inter(a, b) / union(a, b) """
 
-    def __init__(self, p_threshold=0.5, y_threshold=0.01, logits=True):
+    def __init__(self, p_threshold=0.5, y_threshold=0.5):
         """
         Parameters
         ----------
@@ -60,18 +66,16 @@ class IoU_Metric(object):
             Prediction threshold
         y_threshold : float
             Target threshold (set almost every non-zero pixel to 1)
-        logits : bool
-            True if predicted mask comes from logits """
+        """
         self.p_threshold = p_threshold
         self.y_threshold = y_threshold
-        self.logits = logits
-    
+
     def __call__(self, p, y):
         """
         Parameters
         ----------
         p : torch.tensor
-            predicted mask, N x 1 x H x W
+            prediction (logits), N x 1 x H x W
         y : torch.tensor
             ground truth mask, N x 1 x H x W
 
@@ -79,21 +83,17 @@ class IoU_Metric(object):
             inter(a,b) = a * b
             union(a,b) = |a| + |b| - inter
                        = card - inter,        card = |a| + |b|
-                       
-        IoU is defined to be 1.0 when both  'a' and 'b' are empty. """
-        with torch.no_grad():
-            a = torch.sigmoid(p) > self.p_threshold if self.logits else p > self.p_threshold
-            b = y > self.y_threshold    # NOTE: a, b are torch.bool
-            
-            # Consume dimensions 1, 2, 3 => inter is N x 1 tensor
-            inter = torch.sum(a * b, dim=(1,2,3))
-            card = torch.sum(a, dim=(1,2,3)) + torch.sum(b, dim=(1,2,3))
 
-            ret = inter / (card - inter + 1)    # +1 to avoid division by 0
-            ret[card == 0] = 1.0                # when a and b are empty card == 0
-            
+        IoU is defined to be 1.0 when both  'a' and 'b' are empty.
+        """
+        with torch.no_grad():
+            a = p.sigmoid() > self.p_threshold
+            b = y > self.y_threshold    # NOTE: a, b are torch.bool
+
+            ret = IoU(a, b)    # (N, 1, H, W) -> (N, 1)
+
             return ret.mean()
-            
+
     def __repr__(self):
         return 'IoU_Metric()'
 
@@ -103,7 +103,7 @@ class Dice_Metric(object):
 
         Dice(a,b) = 2 * inter(a, b) / (|a| + |b|) """
 
-    def __init__(self, p_threshold=0.5, y_threshold=0.01, logits=True):
+    def __init__(self, p_threshold=0.5, y_threshold=0.5):
         """
         Parameters
         ----------
@@ -111,18 +111,16 @@ class Dice_Metric(object):
             Prediction threshold
         y_threshold : float
             Target threshold (set almost every non-zero pixel to 1)
-        logits : bool
-            True if predicted mask comes from logits """
+        """
         self.p_threshold = p_threshold
         self.y_threshold = y_threshold
-        self.logits = logits
 
     def __call__(self, p, y):
         """
         Parameters
         ----------
         p : torch.tensor
-            predicted mask, N x 1 x H x W
+            prediction (logits), N x 1 x H x W
         y : torch.tensor
             ground truth mask, N x 1 x H x W
 
@@ -131,19 +129,15 @@ class Dice_Metric(object):
             card(a, b) = |a| + |b|
 
         The Dice coefficient is defined to be 1.0 when both 'a' and 'b'
-        are empty. """
+        are empty.
+        """
         with torch.no_grad():
-            a = torch.sigmoid(p) > self.p_threshold if self.logits else p > self.p_threshold
-            b = y > self.y_threshold    # NOTE: a, b are torch.bool
+            a = p.sigmoid() > self.p_threshold    # NOTE: a, b are torch.bool
+            b = y > self.y_threshold
             
-            inter = torch.sum(a * b, dim=(1,2,3))
-            card = torch.sum(a, dim=(1,2,3)) + torch.sum(b, dim=(1,2,3))
+            ret = Dice(a, b)    # (N, 1, H, W) -> (N, 1)
             
-            ret = (2.0*inter) / (card + 1)    # +1 to avoid division by zero
-            ret[card == 0] = 1.0              # when a and b are empty card == 0
-            
-            if self.reduction == 'mean': return ret.mean()
-            else: return ret
+            return ret.mean()
             
     def __repr__(self):
         return 'Dice_Metric()'
--- pytorch_tools/metrics/multi_class.py
+++ pytorch_tools/metrics/multi_class.py
@@ -1,5 +1,4 @@
 import torch
-import numpy as np
 
 # one_hot takes LongTensor with index values of shape (*) and returns
 # a tensor of shape (*, num_classes); e.g. (128x128) -> (128 x 128 x 4)
@@ -15,26 +14,51 @@ def IoU(a, b):
 
     Parameters
     ----------
-    a : np.ndarray or torch.Tensor
-        one-hot encoded labels; H x W x C
-    b : np.ndarray or torch.Tensor
-        one-hot encoded labels; H x W x C
-    # inter, card : N x H x W x C -> N x C
+    a : torch.Tensor
+        one-hot encoded labels; N x H x W x C or H x W x C
+    b : torch.Tensor
+        one-hot encoded labels; N x H x W x C or H x W x C
 
     Returns
     -------
-    float
-        IoU score for each class
+    torch.Tensor
+        IoU score for each class; N x C or C
     """
-    # Transform ndarrays to tensors
-    if type(a) is np.ndarray: a = torch.from_numpy(a)
-    if type(b) is np.ndarray: b = torch.from_numpy(b)
-
-    inter = torch.sum(a * b, dim=(0, 1))
-    card = torch.sum(a, dim=(0, 1)) + torch.sum(b, dim=(0, 1))
+    inter = torch.sum(a * b, dim=(-3,-2))
+    card = torch.sum(a, dim=(-3,-2)) + torch.sum(b, dim=(-3,-2))
 
     ret = inter / (card - inter + 1)    # + 1 to avoid division by 0
     ret[card == 0] = 1.0    # Set to 1.0, where: |a| == 0 and |b| == 0
+
+    return ret
+
+
+def Dice(a, b):
+    """ Dice Score Multi-class
+
+    Compute Dice score for each class; i.e. for inputs (a, b), with shape
+    H x W x C, compute
+        dice[i] = 2*inter(a[...,i], b[...,i]) / (|a[...,i]| + |b[...,i]|)
+    for i in {0, 1, ..., C-1}
+
+    Parameters
+    ----------
+    a : torch.Tensor
+        one-hot encoded labels; N x H x W x C or H x W x C
+    b : torch.Tensor
+        one-hot encoded labels; N x H x W x C or H x W x C
+
+    Returns
+    -------
+    torch.Tensor
+        Dice score for each class; N x C or C
+    """
+    # (N, H, W, C) -> (N, C)    or    (H, W, C) -> (C,)
+    inter = torch.sum(a * b, dim=(-3, -2))    
+    card = torch.sum(a, dim=(-3,-2)) + torch.sum(b, dim=(-3,-2))
+
+    ret = (2.0*inter) / (card + 1)    # +1 to avoid division by zero
+    ret[card == 0] = 1.0              # when a and b are empty card == 0
 
     return ret
 
@@ -43,7 +67,7 @@ class IoU_Metric(object):
     """ IoU metric for multi-class segmentation.
 
     For each class, compute the IoU beween corresponding elements. That is, for
-    a N x num_classes x H x W prediction and target. Compute the IoU:
+    a (N x num_classes x H x W) prediction and target. Compute the IoU:
 
     - for each element in the batch (j)
     - for each class in num_classes (i)
@@ -69,7 +93,6 @@ class IoU_Metric(object):
         per_class : bool
             compute IoU per class (True -> return vector; False -> Scalar)
         """
-        self.softmax = torch.nn.Softmax2d()
         self.num_classes = num_classes
         self.per_class = per_class
 
@@ -87,19 +110,75 @@ class IoU_Metric(object):
             # x = softmax(x) :         N x C x H x W    (probabilities)
             # x = argmax(x, dim=1) :   N x H x W        (labels)
             # x = one_hot(x, C)) :     N x H x W x C    (one-hot encoded)
-            p_hot = one_hot(torch.argmax(self.softmax(pred), dim=1), num_classes=self.num_classes)
+            p_hot = one_hot(pred.softmax(dim=-3).argmax(dim=-3), num_classes=self.num_classes)
             y_hot = one_hot(y, num_classes=self.num_classes)
 
-
-            # inter, card : N x H x W x C -> N x C
-            inter = torch.sum(p_hot * y_hot, dim=(1, 2))
-            card = torch.sum(y_hot, dim=(1, 2)) + torch.sum(p_hot, dim=(1, 2))
-
-            ret = inter / (card - inter + 1)    # + 1 to avoid division by 0
-            ret[card == 0] = 1.0    # Set to 1.0, where: |a| == 0 and |b| == 0
+            ret = IoU(p_hot, y_hot)    # N x C
             
-            if self.per_class: return torch.mean(ret, dim=0)    # N x 1
+            if self.per_class: return torch.mean(ret, dim=0)    # (C,) tensor
             else: return torch.mean(ret)    # 1 x 1 (actually Size[]; i.e. no dims)
         
     def __repr__(self):
         return 'IoU_Metric()'
+
+
+class Dice_Metric(object):
+    """ Dice metric for multi-class segmentation.
+
+    For each class, compute the Dice score beween corresponding elements. That
+    is, for a (N x num_classes x H x W) prediction and target. Compute the
+    Dice:
+
+    - for each element in the batch (j)
+    - for each class in num_classes (i)
+
+        a = j x i x H x W
+        b = j x i x H x W
+
+        Dice(a, b) = 2.0*inter(a, b) / (|a| + |b|)
+
+    This generates a (N x num_classes) tensor with the Dice scores for each
+    class in each batch element. This tensor is then reduced to either:
+
+    - num_classes x 1, tensor containing the average for each class
+    - scalar (tensor), containing the average between all clases.
+    """
+
+    def __init__(self, num_classes, per_class=True):
+        """
+        Parameters
+        ----------
+        num_classes : int
+            number of classes
+        per_class : bool
+            compute IoU per class (True -> return vector; False -> Scalar)
+        """
+        self.num_classes = num_classes
+        self.per_class = per_class
+
+    def __call__(self, pred, y):
+        """
+        Parameters
+        ----------
+        pred : torch.Tensor
+            logits (predicted values): N x num_classes x H x W (float32)
+        y : torch.Tensor
+            target (labels): N x H x W (long)
+        """
+        with torch.no_grad():
+            # x :                      N x C x H x W    (logits)
+            # x = softmax(x) :         N x C x H x W    (probabilities)
+            # x = argmax(x, dim=1) :   N x H x W        (labels)
+            # x = one_hot(x, C)) :     N x H x W x C    (one-hot encoded)
+            # p_hot = one_hot(torch.argmax(self.softmax(pred), dim=1), num_classes=self.num_classes)
+            p_hot = one_hot(pred.softmax(dim=-3).argmax(dim=-3), num_classes=self.num_classes)
+            y_hot = one_hot(y, num_classes=self.num_classes)
+
+            ret = Dice(p_hot, y_hot)    # N x H x W x C -> N x C
+
+            if self.per_class: return torch.mean(ret, dim=0)    # (C,) tensor
+            else: return torch.mean(ret)    # scalar tensor (actually Size[]; i.e. no dims)
+
+    def __repr__(self):
+        return 'Dice_Metric()'
+
